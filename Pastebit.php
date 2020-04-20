a5fb60a1b96f88f39363f91507404c8c8d7e624d
Guillermo Latumahina <glatumahina@roc-dev.com>
2020-04-20 10:35:46 +0200

    first push

---
+++ Pastebit.php
@@ -0,0 +1,44 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#PasteBit</title>
+</head>
+
+<body>
+    <div class="all-content">
+        <div class="top-column">
+            <h1>###Insert Logo###</h1>
+        </div>
+        <div class="form">
+            <div>
+                <p>New Paste</p>
+            </div>
+            <form action="">
+                <div>
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                    <p>Paste Settings</p>
+                </div>
+                <div>
+                    <label for="highlighting">Syntax Highlighting</label>
+                    <select name="highlighting" id="highlighting">
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="pasteName">Paste Name / Title</label>
+                    <input id="pasteName" type="text">
+                </div>
+                <div>
+                    <button type="submit">Create New Paste</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
