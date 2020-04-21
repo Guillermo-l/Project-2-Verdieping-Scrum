5d285db843e414c2a2e128bcdab79cdd6d01c8f8
Its-Kevin <55812435+Its-Kevin@users.noreply.github.com>
2020-04-21 11:54:35 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
+    <link rel="stylesheet" href="https://codepen.io/P1N2O/pen/xxbjYqx.css" />
+    <link rel="stylesheet" href="./style.css" />
+</head>
+
+
+<body>
+    <div class="all-content">
+        <div class="top-column">
+            <img src="img/PasteBit_logo.png" alt="#PasteBit" height="100" width="700">
+        </div>
+
+        <nav>
+            <ul>
+                <li><a href="home.php"> Home</a></li>
+                <li><a href="Code.php"> Type Uw Gewenste Code </a></li>
+                <li><a href="#"> Test code</a></li>
+                <li><a href="About.php"> About</a></li>
+            </ul>
+        </nav>
+
+
+</body>
+
+</html>
